d2ce2ba36c91105d642cae516b78f7327876105e
andrew <andrew@umsl.co.uk>
2026-04-01 18:46:06 +0100

    config updates

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    cpus: 1,
+    staticGenerationMaxConcurrency: 1,
+    staticGenerationMinPagesPerWorker: 1,
+    webpackBuildWorker: false,
+    workerThreads: false,
+  },
   turbopack: {
     root: process.cwd(),
   },
